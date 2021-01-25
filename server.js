b8bdb146b220553c6a15dbaa781f624ee481d811
Hosen Agbaria <50136716+HosenAgbaria@users.noreply.github.com>
2021-01-26 01:58:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,42 +4,6 @@ var cors = require("cors");
 const port = process.env.PORT;
 let articlesAboutDirector = require("./routes/article");
 
-var randomId = function () {
-  // Math.random should be unique because of its seeding algorithm.
-  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-  // after the decimal.
-  return "_" + Math.random().toString(36).substr(2, 9);
-};
-let articlesAboutDirector = [
-  {
-    id: randomId(),
-    articleSource: "כתבה מוואלה",
-    articleSubTitle: "כתבה על שיפור מכירות ובמחסני חשמל ",
-    linkToArticle:
-      "https://www.pinterest.com/pin/461196818093484412/?d=t&mt=signup",
-  },
-  {
-    id: randomId(),
-    articleSource: "כתבה מוואלה",
-    articleSubTitle: "כתבה על שיפור מכירות ובמחסני חשמל ",
-    linkToArticle:
-      "https://www.pinterest.com/pin/461196818093484412/?d=t&mt=signup",
-  },
-  {
-    id: randomId(),
-    articleSource: "כתבה מוואלה",
-    articleSubTitle: "כתבה על שיפור מכירות ובמחסני חשמל ",
-    linkToArticle:
-      "https://www.pinterest.com/pin/461196818093484412/?d=t&mt=signup",
-  },
-  {
-    id: randomId(),
-    articleSource: "כתבה מוואלה",
-    articleSubTitle: "כתבה על שיפור מכירות ובמחסני חשמל ",
-    linkToArticle:
-      "https://www.pinterest.com/pin/461196818093484412/?d=t&mt=signup",
-  },
-];
 app.use(cors());
 app.use(express.json());
 app.get("/", function (req, res) {
